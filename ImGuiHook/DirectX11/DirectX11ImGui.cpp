263d2303ec7c4aa8bd172435fa116bfc904b265c
Chris Andriessen <50437199+ChrisAnd1998@users.noreply.github.com>
2023-04-09 15:58:40 +0200

    Add files via upload

--- ImGuiHook/DirectX11/DirectX11ImGui.cpp
+++ ImGuiHook/DirectX11/DirectX11ImGui.cpp
@@ -115,9 +115,9 @@ void APIENTRY MJDrawIndexed(ID3D11DeviceContext* pContext, UINT IndexCount, UINT
 }
 
 DWORD WINAPI MainThread(LPVOID lpParameter) {
-	//while (!GetAsyncKeyState(VK_F7) & 1) {
-	//	Sleep(1);
-	//}
+	while (!GetAsyncKeyState(VK_F7) & 1) {
+		Sleep(1);
+	}
 	bool WindowFocus = false;
 	while (WindowFocus == false) {
 		DWORD ForegroundWindowProcessID;
